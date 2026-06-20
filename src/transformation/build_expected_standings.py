8a9ff88f402c55abea485026a5cb6b0302c1c85e
Atef Awada <3atef3awada@gmail.com>
2026-06-20 12:49:34 +0200

    created match_probs table

---
+++ src/transformation/build_expected_standings.py
@@ -0,0 +1,4 @@
+"""
+Creates standings based on bookmaker predictions meaning we
+are going to use our odds as match results.
+"""
--- src/transformation/run_transformation.py
+++ src/transformation/run_transformation.py
@@ -8,6 +8,7 @@ from time import perf_counter
 from src.db.engine import engine
 from src.transformation.load_matches_table import load_matches_table
 from src.transformation.build_standings import build_standings
+from src.transformation.build_match_probs import build_match_probs
 from src.transformation.create_transformed_table import create_transformed_table
 
 if __name__ == "__main__":
@@ -18,9 +19,18 @@ if __name__ == "__main__":
     start = perf_counter()
     start_time = datetime.now().isoformat()
     
+    # Load raw data
     raw_matches_df = load_matches_table(engine)
+    
+    # Create standings table
     standings_df = build_standings(raw_matches_df)
     create_transformed_table(standings_df, "standings", engine)
+    print("Created standings table")
+    
+    # Create match_probs table
+    match_probs_df = build_match_probs(raw_matches_df)
+    create_transformed_table(match_probs_df, "match_probs", engine)
+    print("Created match_probs table")
     
     end = perf_counter()
     duration = round(end - start, 5)
