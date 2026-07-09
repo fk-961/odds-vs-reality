45e296fd7584ece3af4aa79b0036f7142bb58d10
Atef Awada <3atef3awada@gmail.com>
2026-07-09 23:59:52 +0200

    fixing gitignore

---
+++ src/core/execution.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from time import perf_counter
+
+class ExecutionTracker:
+    def __enter__(self):
+        self.timestamp = datetime.now().isoformat()
+        self.start = perf_counter()
+        return self
+        
+    def __exit__(self, *_):
+        self.duration = round(
+            perf_counter() - self.start,
+            5
+        )
